f7300c15440f88786cb952228deaea36f5a0a186
imRhytham <rhythamp.404@gmail.com>
2025-05-23 19:22:14 +0530

    added mantine for ui components

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,33 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import type { AppProps } from "next/app";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import theme from "@/styles/theme";
+
+import type { NextPageWithLayout } from "@/types/common";
+
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout;
+};
+
+const App = ({
+	Component,
+	pageProps: { ...pageProps },
+}: AppPropsWithLayout) => {
+	const getLayout = Component.getLayout ?? ((page) => page);
+
+	return (
+		<>
+			<ColorSchemeScript defaultColorScheme="dark" />
+			<MantineProvider theme={theme} defaultColorScheme="dark">
+				<Notifications />
+				{getLayout(<Component {...pageProps} />)}
+			</MantineProvider>
+		</>
+	);
+};
+
+export default App;
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme, } from '@mantine/core';
+
+
+import { mantineColorsPalette } from './color';
+
+const theme = createTheme({
+   colors: mantineColorsPalette,
+   primaryColor: 'primary',
+   fontFamily: "Inter, sans-serif",
+});
+
+export default theme;   
